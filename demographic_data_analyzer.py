290d7a30d2f5086c51230ecdd7ec07ded0d198cc
kchatzigeorgiou <konstantinos.hatzigeorgiou2003@gmail.com>
2026-08-22 11:20:56 +0300

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -2,37 +2,47 @@ import pandas as pd
 
 
 def calculate_demographic_data(print_data=True):
-    # Read data from file
+    # Read data from the csv file
     df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    #It couunts how many times a specific race is mentioned by the column "race" of the csv file
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
+    #It searches all the columns with the "sex" of male and finds the mean age of them keeping 1 decimal on rounding 
     average_age_men = round(df[df['sex']=='Male']['age'].mean(),1)
 
     # What is the percentage of people who have a Bachelor's degree?
+    #It searches all the columns with the "education" of Bachelors and calculates the percentage of it with 1 decimal of rounding
     percentage_bachelors = round((df['education'] == 'Bachelors').mean()*100,1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
+    #We are checking which "education" columns are Bachelors, Masters or Doctorate
     higher_education = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+    #It reverses the True-False List of higher_education parameter
     lower_education = ~higher_education
 
     # percentage with salary >50K
+    #It calculates the percentage with rounding on 1 decimal of people with salary over 50K, depending whether they are highly or not educated
     higher_education_rich = round((df[higher_education]['salary'] == '>50K').mean() * 100, 1)
     lower_education_rich = round((df[lower_education]['salary'] == '>50K').mean()*100,1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
+    #It saves the minimum value of the "hours-per-week" column
     min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
+    #It saves all the workers with the minimum time of working
     num_min_workers = df[df['hours-per-week'] == min_work_hours]
+    #We are checking what percentage of the workers that are working the minimum of time, are getting a salary over 50K
     rich_percentage = round((num_min_workers['salary'] == '>50K').mean() * 100, 1)
 
     # What country has the highest percentage of people that earn >50K?
+    #It divides the people of each country that are getting over 50K salary by the total population of the same country, and finds the maximum value
     highest_earning_country = (df[df['salary'] == '>50K']['native-country'].value_counts()/df['native-country'].value_counts()).idxmax()
     highest_earning_country_percentage = round((df[df['salary'] == '>50K']['native-country'].value_counts()/df['native-country'].value_counts()*100).max(),1)
 
